c2283ccf4fc75db6cf87951ca1b0a187d8fc0881
dx3mod <dx3mod@bk.ru>
2024-03-31 22:58:04 +0300

    add more benchmarks

--- bench/stream_bench.ts
+++ bench/stream_bench.ts
@@ -1,4 +1,4 @@
-import { parseRpmPackage } from "../mod.ts";
+import { InfoTag, OtherTag, parseRpmPackage } from "../mod.ts";
 
 Deno.bench("Read RPM file in chunks", async () => {
   const rpmFile = await Deno.open("hello-2.10-alt1.1.src.rpm", { read: true });
@@ -8,4 +8,20 @@ Deno.bench("Read RPM file in chunks", async () => {
 Deno.bench("Read RPM file with payload in chunks", async () => {
   const rpmFile = await Deno.open("hello-2.10-alt1.1.src.rpm", { read: true });
   await parseRpmPackage(rpmFile.readable, { capture: { payload: true } });
+});
+
+Deno.bench("Read RPM file in chunks with select tags", async () => {
+  const rpmFile = await Deno.open("hello-2.10-alt1.1.src.rpm", { read: true });
+  await parseRpmPackage(rpmFile.readable, {
+    select: {
+      tags: [
+        InfoTag.Name,
+        InfoTag.Os,
+        InfoTag.Arch,
+        InfoTag.Summery,
+        InfoTag.Size,
+        OtherTag.BuildHost,
+      ],
+    },
+  });
 });
