906e7267a260e0fd940be3185e35173e4509dbdc
dx3mod <dx3mod@bk.ru>
2024-04-01 12:37:57 +0300

    add PackageView to export

--- mod.ts
+++ mod.ts
@@ -5,4 +5,6 @@ export * as internal from "./src/internal.mod.ts";
 export { parseRpmPackage, parseRpmPackageSync } from "./src/parser.ts";
 export type { ParseRpmPackageOptions } from "./src/parser.ts";
 
+export { RpmPackageView } from "./src/package_view.ts";
+
 export * from "./src/errors.ts";
